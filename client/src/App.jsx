6893bcfc17287283ce5a3ac61bf1dd4d6bc84230
JainishBorsaniya <jainish.borsaniya303@gmail.com>
2026-03-22 15:39:01 +0530

    setup vite project

---
+++ client/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Welcome to InterviewIQ.AI</h1>
+      <p>Your ultimate interview preparation companion.</p>
+    </div>
+  );
+}
+
+export default App;
